3376eece261c5998e890bfb4bea3d2b3baa2b14b
Duy Ngo <nqn.duy@gmail.com>
2022-06-20 19:10:43 +0700

    final commit

--- dest/main.js
+++ dest/main.js
@@ -1,3 +1,4 @@
+
 /////////////////////////// SCROLL ///////////////////////////////
 $(document).ready(function () {
     let header = $('.header'),
@@ -49,10 +50,16 @@ $(document).ready(function () {
 let clientX, clientY;
 let outerCursor = document.querySelector(".innerCursor");
 let innerCursor = document.querySelector(".outerCursor");
-let title = document.querySelector(".content__title-main");
-let sub = document.querySelector(".content__title-sub");
-
-console.log("title:", title);
+const frame = document.querySelector(".frame");
+const overlayPath = document.querySelector(".overlay__path");
+const menuWrap = document.querySelector(".menu-wrap");
+const menuItems = document.querySelectorAll(".menu__item");
+const openMenuBtn = document.querySelector("button.button-menu");
+const closeMenuBtn = document.querySelector("button.button-close");
+const title = {
+    main: document.querySelector(".content__title-main"),
+    sub: document.querySelector(".content__title-sub"),
+};
 
 let initCursor = () => {
     // add listener to track the current mouse position
@@ -85,7 +92,7 @@ initCursor();
 
 // // hover state
 function growOnHover() {
-    title.addEventListener("mouseover", function () {
+    title.main.addEventListener("mouseover", function () {
         TweenMax.to(innerCursor, 1, {
             width: "300px",
             height: "300px",
@@ -94,11 +101,18 @@ function growOnHover() {
             mixBlendMode: "difference",
         });
     });
+    openMenuBtn.addEventListener("mouseover", function () {
+        TweenMax.to(innerCursor, 1, {
+            width: "64px",
+            height: "64px",
+            transition: "0.005s all",
+        });
+    });
 }
-title.addEventListener("mouseout", function () {
+title.main.addEventListener("mouseout", function () {
     TweenMax.to(innerCursor, 1, {
-        width: "100px",
-        height: "100px",
+        width: "64px",
+        height: "64px",
         background: "transparent",
         transition: "0.01s all",
         mixBlendMode: "normal",
@@ -108,9 +122,151 @@ title.addEventListener("mouseout", function () {
 growOnHover();
 
 var tl = gsap.timeline({delay: 0.4});
-tl.to(title, { autoAlpha: 1, duration: 1.6 });
-tl.from(title, { y: 110, stagger: 0.1, duration: 1 }, "-=0.2");
-tl.to(sub, { autoAlpha: 1, duration: 0.5 }, "-=0.5");
-tl.from(sub, { y: 80, duration: 1, stagger: 0.1 }, "-=0.5");
+tl.to(title.main, { autoAlpha: 1, duration: 1.6 });
+tl.from(title.main, { y: 110, stagger: 0.1, duration: 1 }, "-=0.2");
+tl.to(title.sub, { autoAlpha: 1, duration: 0.5 }, "-=0.5");
+tl.from(title.sub, { y: 80, duration: 1, stagger: 0.1 }, "-=0.5");
 tl.play();
 
+
+
+
+let isAnimating = false;
+
+const openMenu = () => {
+
+    if (isAnimating) return;
+    isAnimating = true;
+    gsap.timeline({
+        onComplete: () => (isAnimating = false),
+    })
+        .set(overlayPath, {
+            attr: { d: "M 0 100 V 100 Q 50 100 100 100 V 100 z" },
+        })
+        .to(
+            overlayPath,
+            {
+                duration: 0.8,
+                ease: "power4.in",
+                attr: { d: "M 0 100 V 50 Q 50 0 100 50 V 100 z" },
+            },
+            0
+        )
+        .to(overlayPath, {
+            duration: 0.3,
+            ease: "power2",
+            attr: { d: "M 0 100 V 0 Q 50 0 100 0 V 100 z" },
+            onComplete: () => {
+                frame.classList.add("frame--menu-open");
+                menuWrap.classList.add("menu-wrap--open");
+            },
+        })
+        .to(
+            [title.main, title.sub],
+            {
+                duration: 0.8,
+                ease: "power3.in",
+                y: -200,
+                stagger: 0.05,
+            },
+            0.2
+        )
+        .set(menuItems, {
+            opacity: 0,
+        })
+        .set(overlayPath, {
+            attr: { d: "M 0 0 V 100 Q 50 100 100 100 V 0 z" },
+        })
+        .to(overlayPath, {
+            duration: 0.3,
+            ease: "power2.in",
+            attr: { d: "M 0 0 V 50 Q 50 0 100 100 V 0 z" },
+        })
+        .to(overlayPath, {
+            duration: 0.8,
+            ease: "power4",
+            attr: { d: "M 0 0 V 0 Q 50 0 100 0 V 0 z" },
+        })
+        .to(
+            menuItems,
+            {
+                duration: 1.1,
+                ease: "power4",
+                startAt: { y: 150 },
+                y: 0,
+                opacity: 1,
+                stagger: 0.05,
+            },
+            ">-=1.1"
+        );
+}
+const closeMenu = () => {
+    if (isAnimating) return;
+    isAnimating = true;
+    gsap.timeline({
+        onComplete: () => (isAnimating = false),
+    })
+        .set(overlayPath, {
+            attr: { d: "M 0 0 V 0 Q 50 0 100 0 V 0 z" },
+        })
+        .to(
+            overlayPath,
+            {
+                duration: 0.8,
+                ease: "power4.in",
+                attr: { d: "M 0 0 V 50 Q 50 100 100 50 V 0 z" },
+            },
+            0
+        )
+        .to(overlayPath, {
+            duration: 0.3,
+            ease: "power2",
+            attr: { d: "M 0 0 V 100 Q 50 100 100 100 V 0 z" },
+            onComplete: () => {
+                frame.classList.remove("frame--menu-open");
+                menuWrap.classList.remove("menu-wrap--open");
+            },
+        })
+        // now reveal
+        .set(overlayPath, {
+            attr: { d: "M 0 100 V 0 Q 50 0 100 0 V 100 z" },
+        })
+        .to(overlayPath, {
+            duration: 0.3,
+            ease: "power2.in",
+            attr: { d: "M 0 100 V 50 Q 50 100 100 50 V 100 z" },
+        })
+        .to(overlayPath, {
+            duration: 0.8,
+            ease: "power4",
+            attr: { d: "M 0 100 V 100 Q 50 100 100 100 V 100 z" },
+        })
+        // title elements
+        .to(
+            [title.main, title.sub],
+            {
+                duration: 1.1,
+                ease: "power4",
+                y: 0,
+                stagger: -0.05,
+            },
+            ">-=1.1"
+        )
+        // menu items translate animation
+        .to(
+            menuItems,
+            {
+                duration: 0.8,
+                ease: "power2.in",
+                y: 100,
+                opacity: 0,
+                stagger: -0.05,
+            },
+            0
+        );
+};
+
+
+
+openMenuBtn.addEventListener('click', openMenu);
+closeMenuBtn.addEventListener("click", closeMenu);
